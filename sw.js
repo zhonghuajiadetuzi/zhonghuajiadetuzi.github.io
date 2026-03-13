ed511d79399340eaa8dec1d843a7b43bd8f84c62
zhonghuajiadetuzi <3222147827@qq.com>
2026-03-13 09:07:24 +0800

    Site updated: 2026-03-13 09:07:23

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1773311418812";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1773364043366";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
